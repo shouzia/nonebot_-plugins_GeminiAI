b15fd8a1c9b5de6a7c059eab0d29fc53355c0c76
shouzia <saopipizhu@163.com>
2024-08-29 10:38:10 +0800

    🎉第一次提交

---
+++ README.md
@@ -0,0 +1,14 @@
+# NoneBot_Google_Gemini_AI插件
+
+1.使用前先去生气Gemini_API的Key
+[申请地址GeminiAPI地址](https://makersuite.google.com/app/apikey?hl=zh-cn)
+
+2.在`__init__.py`中修改Key
+
+3.安装依赖
+
+`pip install pillow google-generativeai`
+
+4.使用方法 @机器人 你的问题
+
+5.注意：国内服务器可能需要使用到魔法手段
---
+++ __init__.py
@@ -0,0 +1,52 @@
+import os
+import google.generativeai as genai
+from nonebot import on_message
+from nonebot.adapters.onebot.v11 import Bot, MessageEvent
+from nonebot.rule import to_me
+
+# 设置 Google Generative AI 的 API Key
+# XXXXXXXXXXXXXXXXXXXXXXXXXXX 修改为你的 API Key
+os.environ['API_KEY'] = 'XXXXXXXXXXXXXXXXXXXXXXXXX'
+genai.configure(api_key=os.environ['API_KEY'])
+
+# 插件的基本信息
+__zx_plugin_name__ = "GoogleAI"
+__plugin_usage__ = """
+usage：
+    GoogleAI
+    指令：
+       @机器人 你的问题
+""".strip()
+__plugin_des__ = "使用 Google Generative AI 的 Nonebot 插件"
+__plugin_cmd__ = ["@机器人"]
+__plugin_version__ = 0.1
+__plugin_author__ = 'Shouzi'
+__plugin_settings__ = {
+    "level": 5,
+    "default_status": True,
+    "limit_superuser": False,
+    "cmd": ["@机器人"],
+}
+
+# 初始化消息处理器，优先级设为100
+google_ai = on_message(rule=to_me(), priority=100)
+
+@google_ai.handle()
+async def handle_message(bot: Bot, event: MessageEvent):
+    user_input = str(event.message).strip()  # 获取并去除消息的首尾空格
+    if not user_input:
+        return
+
+    # 调用 Google Generative AI 生成内容
+    answer = generate_content(user_input)
+    await google_ai.finish(answer)
+
+def generate_content(prompt):
+    # 初始化模型
+    model = genai.GenerativeModel('gemini-1.5-flash')
+
+    # 生成内容
+    response = model.generate_content(prompt+",并使用中文回答我!")
+    
+    # 返回生成的文本内容
+    return response.text
