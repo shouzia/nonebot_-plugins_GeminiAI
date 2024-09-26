b0d3a6e59fd489fb8cb8aca46ea62a12dacaf6fb
shouzia <saopipizhu@163.com>
2024-09-26 16:41:39 +0800

    🐛

--- __init__.py
+++ __init__.py
@@ -10,46 +10,46 @@ os.environ['API_KEY'] = 'XXXXXXXXXXXXXXXXXXXXXXXXX'
 genai.configure(api_key=os.environ['API_KEY'])
 
 # 插件的基本信息
-# __zx_plugin_name__ = "GoogleAI"
-# __plugin_usage__ = """
-# usage：
-#     GoogleAI
-#     指令：
-#        @机器人 你的问题
-# """.strip()
-# __plugin_des__ = "使用 Google Generative AI 的 Nonebot 插件"
-# __plugin_cmd__ = ["@机器人"]
-# __plugin_version__ = 0.1
-# __plugin_author__ = 'Shouzi'
-# __plugin_settings__ = {
-#     "level": 5,
-#     "default_status": True,
-#     "limit_superuser": False,
-#     "cmd": ["@机器人"],
-# }
+__zx_plugin_name__ = "GoogleAI"
+__plugin_usage__ = """
+usage：
+    GoogleAI
+    指令：
+       @机器人 你的问题
+""".strip()
+__plugin_des__ = "使用 Google Generative AI 的 Nonebot 插件"
+__plugin_cmd__ = ["@机器人"]
+__plugin_version__ = 0.1
+__plugin_author__ = 'Shouzi'
+__plugin_settings__ = {
+    "level": 5,
+    "default_status": True,
+    "limit_superuser": False,
+    "cmd": ["@机器人"],
+}
 
 
-__plugin_meta__ = PluginMetadata(
-    name="Google_Gemini_AI",
-    description="使用 Google Generative AI 的 Nonebot 插件",
-    usage="""
-    @机器人 你的问题
-    示例: @机器人 你的问题
-    """.strip(),
-    extra=PluginExtraData(
-        author="shouzi",
-        version="0.1",
-        # configs=[
-        #     RegisterConfig(
-        #         key="WITHDRAW_COS_MESSAGE",
-        #         value=(0, 1),
-        #         help="自动撤回，参1：延迟撤回色图时间(秒)，0 为关闭 | 参2：监控聊天类型，0(私聊) 1(群聊) 2(群聊+私聊)",
-        #         default_value=(0, 1),
-        #         type=Tuple[int, int],
-        #     ),
-        # ],
-    ).dict(),
-)
+# __plugin_meta__ = PluginMetadata(
+#     name="Google_Gemini_AI",
+#     description="使用 Google Generative AI 的 Nonebot 插件",
+#     usage="""
+#     @机器人 你的问题
+#     示例: @机器人 你的问题
+#     """.strip(),
+#     extra=PluginExtraData(
+#         author="shouzi",
+#         version="0.1",
+#         configs=[
+#             RegisterConfig(
+#                 key="WITHDRAW_COS_MESSAGE",
+#                 value=(0, 1),
+#                 help="自动撤回，参1：延迟撤回色图时间(秒)，0 为关闭 | 参2：监控聊天类型，0(私聊) 1(群聊) 2(群聊+私聊)",
+#                 default_value=(0, 1),
+#                 type=Tuple[int, int],
+#             ),
+#         ],
+#     ).dict(),
+# )
 
 # 初始化消息处理器，优先级设为100
 google_ai = on_message(rule=to_me(), priority=100)
